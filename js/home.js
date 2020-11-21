890646821858ed7d6f9c583b6f9be225a080ae85
sreeja99 <srijagodishala@gmail.com>
2020-11-21 18:11:38 +0530

    Added:Feature To Populate From Local Storage

--- js/home.js
+++ js/home.js
@@ -1,36 +1,37 @@
+let contactDetailsList;
 window.addEventListener('DOMContentLoaded',(event)=>{
+    contactDetailsList = getContactDetailsFromLocalStorage();
+    document.querySelector(".contact-count").textContent = contactDetailsList.length;
     createInnerHtml();
-  });
+});
   
-  const createInnerHtml = () => {
-      const headerHtml = "<th></th><th>Fullname</th><th>Address</th><th>City</th>"
-                          +"<th>State</th><th>Zip Code</th><th>Phone Number</th>";
-      let innerHtml = `${headerHtml}`;
-      let addressBookList = createContactsJSON();
-      for(const contact of addressBookList){
-        innerHtml = `${innerHtml}
-      <tr><td></td>
-      <td>${contact._name}</td>
-      <td>${contact._address}</td>
-      <td>${contact._city}</td>
-      <td>${contact._state}</td>
-      <td>${contact._zip}</td>
-      <td>${contact._phone}</td>
-      <td>
-          <img id="1" onclick="remove(this)" alt="delete"
-              src="../assets/icons/delete-black-18dp.svg">
-          <img id="1" alt="edit" onclick="update(this)" 
-              src="../assets/icons/create-black-18dp.svg">
-      </td>
-      </tr>
-      `;
-      }
-      document.querySelector('#table-display').innerHTML = innerHtml;
-  }
-  const createContactsJSON = () => {
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Fullname</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>Phone Number</th>";
+    let innerHtml = `${headerHtml}`;
+    for(const contact of contactDetailsList){
+    innerHtml = `${innerHtml}
+    <tr><td></td>
+    <td>${contact._name}</td>
+    <td>${contact._address}</td>
+    <td>${contact._city}</td>
+    <td>${contact._state}</td>
+    <td>${contact._zip}</td>
+    <td>${contact._phone}</td>
+    <td>
+    <img id="1" onclick="remove(this)" alt="delete"
+        src="../assets/icons/delete-black-18dp.svg">
+    <img id="1" alt="edit" onclick="update(this)" 
+        src="../assets/icons/create-black-18dp.svg">
+    </td>
+    </tr>
+    `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+const createContactsJSON = () => {
     let contactList = [
         {
-            _name: "Sreeja",
+            _name: "Mark",
             _address: "E-31/19",
             _city: "Bangalore",
             _state: "Karanataka",
@@ -38,7 +39,7 @@ window.addEventListener('DOMContentLoaded',(event)=>{
             _phone: "9873599502"
         },
         {
-          _name: "Shreya",
+          _name: "Bill",
           _address: "E-31/19",
           _city: "Bangalore",
           _state: "Karanataka",
@@ -47,4 +48,8 @@ window.addEventListener('DOMContentLoaded',(event)=>{
       }
     ];
     return contactList;
+}
+const getContactDetailsFromLocalStorage = () => {
+    return localStorage.getItem('ContactList')?
+                                JSON.parse(localStorage.getItem('ContactList')):[];
   }
