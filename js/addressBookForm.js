c87b1eb81ea85e08ba3f45ccd993f1607605ffbd
sreeja99 <srijagodishala@gmail.com>
2020-11-21 17:39:27 +0530

    Added:Handled Reset And Cancel Button

--- js/addressBookForm.js
+++ js/addressBookForm.js
@@ -33,6 +33,14 @@ const save = ()=>{
     }
     alert(contactObj.toString());
 }
+const resetForm = ()=>{
+    document.querySelector("#name").value = "";
+    document.querySelector('#phone').value = "";
+    document.querySelector('#address').value= "";
+    document.querySelector('#city').value = "";
+    document.querySelector('#state').value= "";
+    document.querySelector('#zip').value= "";
+}
 const setContactObject = ()=>{
     try{
         contactObj._name = document.querySelector('#name').value;
@@ -54,4 +62,7 @@ const createAndUpdateStorage = ()=>{
     }
     alert(contactList.toString())
     localStorage.setItem("ContactList",JSON.stringify(contactList))
+}
+const cancel = ()=>{
+    window.location.replace(site_properties.home_page);
 }
